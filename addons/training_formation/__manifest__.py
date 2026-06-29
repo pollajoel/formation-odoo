466428d8e45f1f7cc9fe630912fa1ef6c3817075
pollajoel <pollajoel2017@gmail.com>
2026-06-30 00:47:38 +0200

    feat:(arch) architecture d'un module odoo

---
+++ addons/training_formation/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'training_formation',
+    'summary': """
+        module de gestion des formation    
+    """,
+    'descripton': """
+        ce module va permettre de gérer les formations en mettant en relations 
+        les formateurs et les apprenants
+    """,
+    'installable': True,
+    'application': True,
+    'author': 'joël',
+    'category': 'training management',
+    'version': '0.1',
+    'license': 'LGPL-3', # OEEL-1 =>module entreprise, OPL-1=> Licence propietaire
+    'auto_install': False,
+    'depends': ['contacts'],
+    'data': []
+}
